170577873c6de0db5aceaa169ba271166c34c4d3
Maniacal <mike.glenney@gmail.com>
2018-02-22 15:49:57 -0700

    Some more updates

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### Instructions
+
+```
+mkdir <somedir>
+cd <somedir>
+wget https://github.com/mmayden/mintyFresh/archive/master.tar.gz
+tar -xzvf master.tar.gz
+bash mintyFresh.sh
+```
--- mintyFresh.sh
+++ mintyFresh.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-#MintyFresh
+# 
+
+echo '
+   _____  .__        __          ___________                     .__     
+  /     \ |__| _____/  |_ ___.__.\_   _____/______   ____   _____|  |__  
+ /  \ /  \|  |/    \   __<   |  | |    __) \_  __ \_/ __ \ /  ___/  |  \ 
+/    Y    \  |   |  \  |  \___  | |     \   |  | \/\  ___/ \___ \|   Y  \
+\____|__  /__|___|  /__|  / ____| \___  /   |__|    \___  >____  >___|  /
+        \/        \/      \/          \/                \/     \/     \/
+'
 
 # Update/upgrade
 echo "Initiating Minty Fresh..."
@@ -12,17 +21,17 @@ apt-get install vim -y
 mkdir -p "$HOME/bin"
 
 # Create vim config file (.vimrc)
-cp ./vimrc "$HOME/.vimrc"
+cp .files/vimrc "$HOME/.vimrc"
 chmod 0600 "$HOME/.vimrc"
 
 # Copy in background image
 bg_file="background.jpg"
 bg_location="$HOME/Pictures/$bg_file"
-cp "$bg_file" "$bg_location"
+cp "./files/$bg_file" "$bg_location"
 
-###Changing desktop background doesn't work yet. It worked once and then I deleted
-#any of the newly created files so I could make a quick change to the line.
-#Something happened and now every time I've tried it its not working
+# Changing desktop background doesn't work yet. It worked once and then I deleted
+# any of the newly created files so I could make a quick change to the line.
+# Something happened and now every time I've tried it its not working
 gsettings set org.gnome.desktop.background picture-uri "file://$bg_location"
 
 # Install/setup git+github acc
@@ -30,5 +39,5 @@ add-apt-repository -y ppa:git-core/ppa # apt update; apt install git
 apt-get update && upgrade -y
 apt-get install -y git
 
-cp ./gitconfig "$HOME/.gitconfig"
+cp ./files/gitconfig "$HOME/.gitconfig"
 chmod 0600 "$HOME/.gitconfig"
